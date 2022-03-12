fde5400e1ba1ea29b2ac469317ec0943b6641d4c
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2022-03-12 13:35:03 +0530

    del m

--- index.js
+++
@@ -1,7 +0,0 @@
-import TrackPlayer from 'react-native-track-player';
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
-TrackPlayer.registerPlaybackService(() => require('./service.js'));
